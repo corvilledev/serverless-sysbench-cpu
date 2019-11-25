44bf2ac45f161828289aa2b9cb56faa142742a1a
Anthony Monthe <anthony.monthe@gmail.com>
2019-11-25 00:54:00 +0000

    1st commit

---
+++ run.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+iterations=${iterations:-3}
+threads="1 2 4 8"
+
+for i in $(seq 1 $iterations) ; do
+    for thread in $threads ; do
+        sysbench --threads=$thread --time=30 cpu --cpu-max-prime=64000 run | tee output-$i-$thread.txt
+        cb-client sysbench_cpu < output-$i-$thread.txt
+    done
+done
