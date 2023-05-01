8a03925a2aae0e5f7aba80ffa6bc15f7b44d5d05
corvilledev <corvilledev@gmail.com>
2023-05-01 20:26:32 +0000

    feat: updated bench functions

--- google/main.py
+++ google/main.py
@@ -1,6 +1,9 @@
 import json
+import importlib.util
+import pkg_resources
+import os
+from pathlib import Path
 from flask import jsonify, request
-from pyperformance.run import run_benchmarks
 
 def run_sysbench(request):
     try:
@@ -8,80 +11,31 @@ def run_sysbench(request):
         benchmark_name = request.args.get('benchmark', 'bm_telco')
 
         # Validate the benchmark name
-        allowed_benchmarks = [
-            'bm_2to3', 
-            'bm_async_generators', 
-            'bm_async_tree',
-            'bm_asyncio_tcp',
-            'bm_chameleon',
-            'bm_chaos',
-            'bm_comprehensions',
-            'bm_concurrent_imap',
-            'bm_coroutines',
-            'bm_coverage',
-            'bm_crypto_pyaes',
-            'bm_dask',
-            'bm_deepcopy',
-            'bm_deltablue',
-            'bm_django_template',
-            'bm_docutils',
-            'bm_dulwich_log',
-            'bm_fannkuch',
-            'bm_float',
-            'bm_gc_collect',
-            'bm_gc_traversal',
-            'bm_generators',
-            'bm_genshi',
-            'bm_go',
-            'bm_hexiom',
-            'bm_hg_startup',
-            'bm_html5lib',
-            'bm_json_dumps',
-            'bm_json_loads',
-            'bm_logging',
-            'bm_mako',
-            'bm_mdp',
-            'bm_meteor_contest',
-            'bm_nbody',
-            'bm_nqueens',
-            'bm_pathlib',
-            'bm_pickle',
-            'bm_pidigits',
-            'bm_pprint',
-            'bm_pyflate',
-            'bm_python_startup',
-            'bm_raytrace',
-            'bm_regex_compile',
-            'bm_regex_dna',
-            'bm_regex_effbot',
-            'bm_regex_v8',
-            'bm_richards',
-            'bm_richards_super',
-            'bm_scimark',
-            'bm_spectral_norm',
-            'bm_sqlalchemy_declarative',
-            'bm_sqlalchemy_imperative',
-            'bm_sqlglot',
-            'bm_sqlite_synth',
-            'bm_sympy',
-            'bm_telco',
-            'bm_tomli_loads',
-            'bm_tornado_http',
-            'bm_typing_runtime_protocols',
-            'bm_unpack_sequence',
-            'bm_xml_etree'                     
-        ]
+        benchmarks_path = pkg_resources.resource_filename('pyperformance', 'data-files/benchmarks')
+        allowed_benchmarks = [p.name for p in Path(benchmarks_path).iterdir() if p.is_dir()]
 
         if benchmark_name not in allowed_benchmarks:
             return jsonify({"status": "error", "message": f"Invalid benchmark name. Allowed benchmarks: {', '.join(allowed_benchmarks)}"})
+        
+        # Load the chosen benchmark
+        benchmark_file = os.path.join(benchmarks_path, benchmark_name, 'run_benchmark.py')
+        spec = importlib.util.spec_from_file_location(f"{benchmark_name}_module", benchmark_file)
+        benchmark_module = importlib.util.module_from_spec(spec)
+        spec.loader.exec_module(benchmark_module)
 
-        # Run the pyperformance benchmarks
-        result = run_benchmarks(benchmarks=[benchmark_name], options=None)
+        # Run the chosen benchmark
+        def run_benchmark(loops=1):
+            filename = os.path.join(benchmarks_path, benchmark_name, "data", "telco-bench.b")
+            return benchmark_module.bench_telco(loops, filename)
+
+        result = run_benchmark()
 
         # Process the results
-        result_json = json.dumps(result.as_dict(), indent=4)
+        result_dict = {"benchmark": benchmark_name, "result": result}
+        result_json = json.dumps(result_dict, indent=4)
 
         return jsonify({"status": "success", "message": f"Pyperformance benchmark '{benchmark_name}' has been executed.", "results": result_json})
 
     except Exception as e:
         return jsonify({"status": "error", "message": str(e)})
+
